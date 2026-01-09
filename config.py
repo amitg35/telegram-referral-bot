afa6bd0ddb1d81f77626d44af1f2c4ff0c637a0b
amitg35 <amitg40324@gmail.com>
2026-01-09 21:35:08 +0530

    Add configuration for bot and channels

---
+++ config.py
@@ -0,0 +1,19 @@
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+MONGO_URL = "YOUR_MONGODB_URL"
+
+FORCE_CHANNELS = [
+    "@channel1",
+    "@channel2",
+    "@channel3",
+    "@channel4"
+]
+
+JOIN_LINKS = [
+    "https://t.me/+imnY9aNAt9A1YzNl",
+    "https://t.me/+ZlHW_ZUS6yQ1NDA9",
+    "https://t.me/+Db38DSH0iR1iMDc1",
+    "https://t.me/+1ZMyGGyTb1s0MzM1"
+]
+
+ADMIN_IDS = [123456789]  # apna Telegram ID
+DAILY_BONUS = 5
